2c7e247238463988e0a5b974e920b26ee0a42f02
yurissssss <yur1n9@kakao.com>
2025-04-09 11:20:32 +0900

    Fix : 'db.json' BASE_URL로 받아오기 04-09

--- AccountBook/src/pages/TransactionPage.vue
+++ AccountBook/src/pages/TransactionPage.vue
@@ -1,11 +1,89 @@
 <script setup>
+import { ref, computed, onMounted } from 'vue'
+import axios from 'axios'
 
+// BASE_URI (db.json)
+import { BASE_URI } from '@/constants/api'
+
+// 필터/검색
+import FilterCategory from '@/components/filter_search/FilterCategory.vue'
+import SearchByMemo from '@/components/filter_search/SearchByMemo.vue'
+
+import {
+  INCOME_CATEGORIES,
+  EXPENSE_CATEGORIES,
+  CATEGORY_MAP,
+} from '@/constants/categories'
+
+// 초기값 설정 (카테고리 필터 선택 전 상태)
+const categorySelected = ref('all')
+// 초기값 설정 (검색창 입력 전 상태)
+const memoInputted = ref('')
+// 거래내역 담아올 배열 정의
+const transactions = ref([])
+
+// db.json에서 거래내역 불러오기
+const fetchTransactions = async () => {
+  const res = await axios.get(`${BASE_URI}/transactions`)
+  transactions.value = res.data.map(item => ({
+    ...item,
+    category: item.category?.toLowerCase() || '', // 카테고리를 소문자로
+  }))
+}
+
+onMounted(fetchTransactions)
+
+// 카테고리 선택 핸들러
+const CategoryChangeHandler = category => {
+  // FilteredCategory.vuse에서 emit된 값 받아서 저장
+  categorySelected.value = category
+}
+
+// 검색창 입력 핸들러
+const MemoSearchHandler = text => {
+  memoInputted.value = text.toLowerCase().trim()
+}
+
+// 카테고리 필터링된 내역 저장 : 카테고리 + 메모
+const filteredList = computed(() => {
+  return transactions.value.filter(item => {
+    const categoryMatch =
+      categorySelected.value === 'all' ||
+      item.category === categorySelected.value
+
+    const memoMatch =
+      memoInputted.value === '' ||
+      (item.description &&
+        item.description.toLowerCase().includes(memoInputted.value))
+    return categoryMatch && memoMatch
+  })
+})
 </script>
 
 <template>
-  <div class="TransactionPage">수입지출페이지</div>
+  <div class="TransactionPage">
+    <!-- 카테고리 필터 -->
+    <FilterCategory
+      :categories="EXPENSE_CATEGORIES"
+      @categorySelected="CategoryChangeHandler"
+    />
+    <!-- 메모 검색 -->
+    <SearchByMemo @memoInputted="MemoSearchHandler" />
+
+    <!-- 필터링된 내역 출력 -->
+    <ul class="filtered_list">
+      <!-- 수정 필요 -->
+      <li v-for="filtered in filteredList" :key="filtered.id">
+        <!-- 카테고리 원본: {{ filtered.category }} 매핑된 한글:
+        {{ CATEGORY_MAP[filtered.category] || filtered.category }} -->
+
+        {{ filtered.date }} -
+        {{ CATEGORY_MAP[filtered.category] || filtered.category }}
+        - {{ filtered.description }} - {{ filtered.amount }}
+      </li>
+    </ul>
+    <p v-if="filteredList.length === 0">표시할 내역이 없습니다.</p>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
