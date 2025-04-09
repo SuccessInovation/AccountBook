341ebbf40e8efc016a71687a51a6546a46513e34
lolddong <w2crosss@gmail.com>
2025-04-09 09:16:49 +0900

    Design: TheFooter.vue 완료

--- src/layout/TheFooter.vue
+++ src/layout/TheFooter.vue
@@ -1,10 +1,27 @@
 <script setup></script>
 
 <template>
-  <div class="TheFooter"></div>
+  <div class="TheFooter">
+    <span id="copyright"
+      >copyright &copy 2025 성공하면 혁신!조. All rights reserved.</span
+    >
+  </div>
 </template>
 
 <style scoped>
 .TheFooter {
+  color: var(--font-color);
+  position: fixed;
+  bottom: 0;
+  left: 300px;
+  width: calc(100% - 300px); /* 사이드바 너비만큼 뺌 */
+  height: 80px;
+  display: flex;
+  align-items: center;
+  border-top: 0.5px solid var(--dark-brown);
+  padding-right: 30px;
+}
+#copyright {
+  margin-left: auto;
 }
 </style>
