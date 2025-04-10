4cb26c7ab1489e6ae4e0aa49330acb458d92f26b
Pearl <w2crosss@gmail.com>
2025-04-11 07:35:58 +0900

    Merge: merging

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-<!-- <template>
-  <div class="container">
-    <TheHeader />
-    <div class="content_main">
-=======
-<template>
-  <div class="container">
-    <TheHeader />
-<<<<<<< HEAD
-    <div class="main-content">
->>>>>>> d7a5d5b (Design: TheHeader.vue 완료)
-=======
-    <div class="content_main">
->>>>>>> b029045 (Design: Header, SNB, Footer 완료)
-      <TheSidebar />
-      <div class="container_router_view">
-        <router-view />
-      </div>
-    </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <TheFooter />
-  </div>
-=======
-  </div>
-  <TheFooter />
->>>>>>> d7a5d5b (Design: TheHeader.vue 완료)
-=======
-    <TheFooter />
-  </div>
->>>>>>> b029045 (Design: Header, SNB, Footer 완료)
-</template>
-
-<script>
-import TheHeader from './layout/TheHeader.vue'
-import TheSidebar from './layout/TheSidebar.vue'
-import TheFooter from './layout/TheFooter.vue'
-
-export default {
-  components: {
-    TheHeader,
-    TheSidebar,
-    TheFooter,
-  },
-}
-</script>
-
-<style>
-.container {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content_main {
-  display: flex;
-  flex: 1;
-  margin-top: 120px; /* 헤더 높이 */
-<<<<<<< HEAD
-  position: relative;
-}
-.container_router_view {
-  display: flex;
-  left: 250px;
-}
-</style> -->
 <template>
   <div :style="{ backgroundColor: backgroundColor }" class="app_wrap">
     <div v-if="layout === 'none'">
@@ -113,8 +45,6 @@ const backgroundColor = computed(() => route.meta.backgroundColor || '#fff')
   display: flex;
   flex: 1;
   margin-top: 120px; /* 헤더 높이 */
-=======
->>>>>>> b029045 (Design: Header, SNB, Footer 완료)
 }
 .container_router_view {
   flex: 1;
