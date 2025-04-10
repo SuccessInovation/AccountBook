f1a22cbf23ce3417990a7bbbe8b182d54120e5f5
Yujung Choi <yujung5548@naver.com>
2025-04-10 23:59:07 +0900

    Revert "Merge remote-tracking branch 'origin/feature/2-transaction-filter' into feature/2-transaction-calendar"
    This reverts commit 233e7afa3e3d4b54a8d48a842b475c0b8996f9d4, reversing
    changes made to 103df08a68e96be0c15ccf571066227fad078754.

--- src/router/index.js
+++ src/router/index.js
@@ -47,11 +47,6 @@ const routes = [
     meta: { layout: 'default' }, //  헤더/사이드바 있는 레이아웃
   },
   {
-    path: '/transaction/edit/:id', // 동적 파라미터 :id
-    name: 'TransactionEdit',
-    component: TransactionEdit,
-  },
-  {
     path: '/popup',
     name: 'Popup',
     component: PopupPage,
